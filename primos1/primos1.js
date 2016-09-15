01a15885b8b95564770ebabf712b8db7351c3f9e
Adolfo Aparicio <financieras@gmail.com>
2016-09-15 22:34:01 +0200

    Update primos1.js

--- primos1/primos1.js
+++ primos1/primos1.js
@@ -1,10 +1,10 @@
  document.write('<h1>'+'Generación de números primos hasta 1.000.000'+'</h1>');  
- var texto=2 + "<br>";  
- var primo  
- for (i=3;i<=10000000;i=i+2){  
-     primo=true  
+ var texto=2 + "<br>";          //el 2 es primo, y todos los demás pares no lo son
+ var primo
+ for (i=3;i<=10000000;i=i+2){   //tratamos solo los impares 
+     primo=true;                //inicialmente un número es primo hasta que no se demuestre lo contrario  
      for(j=3;j<Math.sqrt(i);j=j+2){  
-         if (i%j===0){  
+         if (i%j==0){  
              primo=false;  
              break;  
          }  
