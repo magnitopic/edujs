8c3e14835270703a3f05ff320ebc931210941baa
magnitopic <magnitopic@gmail.com>
2022-04-14 23:50:06 +0200

    Tablas complejas 2022

---
+++ tablasComplejas2.0/script.js
@@ -0,0 +1,17 @@
+const input = document.querySelector("#result");
+
+document.querySelector("#one").addEventListener("click", (e) => {
+	input.value = "1";
+});
+
+document.querySelector("#two").addEventListener("click", (e) => {
+	input.value = "2";
+});
+
+document.querySelector("#three").addEventListener("click", (e) => {
+	input.value = "3";
+});
+
+document.querySelector("#four").addEventListener("click", (e) => {
+	input.value = "4";
+});
