150611baf84966a27480aff4b0e83551a51de351
magnitopic <magnitopic@gmail.com>
2022-08-23 12:53:27 +0200

    Basic structure for Mothy Hall

---
+++ MontyHall/main.js
@@ -0,0 +1,14 @@
+doors = [0, 0, 0];
+doors[parseInt(Math.random() * 3)] = 1;
+
+/* document.querySelectorAll(".doors").forEach((element) => {
+	element.addEventListener("click", (e) => {
+		e.target.style.backgroundColor = "blue";
+	});
+});
+
+document.querySelector("#reset").addEventListener("click", (e) => {
+	document.querySelectorAll(".doors").forEach((element) => {
+		element.style.backgroundColor = "red";
+	});
+}); */
