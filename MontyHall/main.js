7bb5eabf383c565441e9946742ab98d4f0553b6e
magnitopic <magnitopic@gmail.com>
2022-08-23 18:06:47 +0200

    chnage of picture

--- MontyHall/main.js
+++ MontyHall/main.js
@@ -1,14 +1,30 @@
 doors = [0, 0, 0];
 doors[parseInt(Math.random() * 3)] = 1;
+console.log(doors);
+round = 0;
 
-/* document.querySelectorAll(".doors").forEach((element) => {
+document.querySelectorAll(".doors").forEach((element, i) => {
 	element.addEventListener("click", (e) => {
-		e.target.style.backgroundColor = "blue";
+		changeImages(i);
 	});
 });
 
 document.querySelector("#reset").addEventListener("click", (e) => {
 	document.querySelectorAll(".doors").forEach((element) => {
-		element.style.backgroundColor = "red";
+		element.src = "imgs/door.png";
+		round = 0;
 	});
-}); */
+});
+
+const changeImages = (clickedDoor) => {
+	if (!round) {
+		e.target.src = "imgs/donky.png";
+		do {
+			rnd = parseInt(Math.random() * 3);
+		} while (doors[rnd] || rnd === clickedDoor);
+		document.querySelectorAll(".doors")[rnd].src = "imgs/donky.png";
+	} else if (round < 2) {
+		e.target.src = "imgs/car.png";
+	}
+	round++;
+};
